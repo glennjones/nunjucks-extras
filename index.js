466f8102be94036b0522863edc4d7621bea4c937
Glenn Jones <glennjonesnet@gmail.com>
2016-07-02 23:02:26 +0100

    Fix name issue

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function append( Nunjucks, env ){
     TagMarkdown.register(env, Marked);
     TagTrim.register(env);
     TagSwitch.register(env);
-    TagTranlate.register(env);
+    TagTranslate.register(env);
 
     Nunjucks.nodes.Include = Nunjucks.nodes.Node.extend('Include', { fields: ['template', 'ignoreMissing', 'with'] });
     TagIncludeWith.register(env);
